defe0d383aef7ced5693e1aaadb87de5c6b2a674
bourbonut <ben.bourbon06@gmail.com>
2022-05-25 10:47:33 +0200

    Add test for REINFORCE algorithm

--- tests/test_reinforce.py
+++ tests/test_reinforce.py
@@ -1,4 +1,11 @@
 from evaluator.model import *
+import gym
+
+env = gym.make("CartPole-v0")
+Estimator.NHIDDEN = 16
+ninput = env.observation_space.shape[0]
+noutput = env.action_space.n
+agent = Estimator(ninput, noutput).to(device)
 
 
 def func(x):
@@ -7,3 +14,17 @@ def func(x):
 
 def test_cannary():
     assert func(3) == 4
+
+
+def test_neural_network():
+    assert agent.input.in_features == 4
+    assert agent.input.out_features == 16
+    assert agent.output.in_features == 16
+    assert agent.output.out_features == 2
+
+
+def test_reinforce():
+    results = reinforce(env, agent)
+    assert results["ep"][-1] == 2000
+    assert len(results["ep"]) == 2000
+    assert max(results["avg_rewards"]) > 180
