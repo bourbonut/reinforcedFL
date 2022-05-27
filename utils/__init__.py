a5e6c0b66d8f9dd51327c843ee09ff5bc611dbd3
bourbonut <ben.bourbon06@gmail.com>
2022-05-27 10:22:22 +0200

    Add path functions

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .path import *
+from .partition import generate_IID_parties
---
+++ utils/path.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+ROOT_PATH = Path().absolute()
+EXP_PATH = ROOT_PATH / "experiments"
+DATA_PATH = Path().absolute() / "data"
+
+
+def create(path):
+    if not (path.exists()):
+        path.mkdir()
+
+
+def iterate(path):
+    folders = list(path.iterdir())
+    index = 1
+    while any(path.glob(f"experiment-{index}*")):
+        index += 1
+
+    return path / f"experiment-{index}"
