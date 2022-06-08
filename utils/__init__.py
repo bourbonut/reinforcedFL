949b09c89ddf50a09cde74dc236b24fdcf46539c
bourbonut <ben.bourbon06@gmail.com>
2022-06-08 09:48:32 +0200

    Add plot

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,9 @@
 Module for all functions useful for :
 - partitioning
 - managing path
+- plotting
 """
 
 from .path import *
 from .partition import generate_IID_parties
+from .plot import lineXY
---
+++ utils/plot.py
@@ -0,0 +1,29 @@
+import pygal
+
+
+def lineXY(y, filename, x=None, title="", **kwargs):
+    """
+    Save a line chart
+
+    Parameters:
+        y (dict or list):               y values (if `dict`, serie name as key
+                                        and data as value)
+        filename (str or PosixPath):    name of the file or full path
+        x (list):                       x values
+        title (str):                    title of the chart
+    """
+    line_chart = pygal.XY(**kwargs)
+    line_chart.title = title
+    if isinstance(y, dict):
+        for serie in y:
+            if x is None:
+                x = list(range(len(y[serie])))
+            line_chart.add(serie, list(zip(x, y[serie])))
+    elif isinstance(y, list):
+        if x is None:
+            x = list(range(len(y)))
+        for data in y:
+            line_chart.add("", list(zip(x, data)))
+    else:
+        raise TypeError("`y` must be a `dict` or a `list`.")
+    line_chart.render_to_png(str(filename))
