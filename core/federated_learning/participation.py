Update normalization and fixed bugs

core/federated_learning/participation.py
@@ -24,7 +24,10 @@ class Scheduler:
 
     def normalize(self, state, flatten=True):
         state = torch.tensor(state, dtype=torch.float).view(-1, self.k)
-        state = state / torch.norm(state, dim=0)
+        mean = torch.cat([state.mean(1).unsqueeze(0)] * state.size(1)).T
+        std = torch.cat([state.std(1).unsqueeze(0)] * state.size(1)).T
+        state = (state - mean) / std
+        #state = state / torch.norm(state, dim=0)
         return state.flatten() if flatten else state
 
     def select_next_partipants(self, state):
@@ -59,4 +62,5 @@ class Scheduler:
         self.rewards.clear()
         self.action = None
         self.i += 1
-        self.agent.losses.clear()
+        self.agent.losses[0] = 0
+        self.agent.losses[1] = 0

core/scheduler/model.py
@@ -59,7 +59,6 @@ class ActorCritic:
         probas = self.actor(state)
         action = torch.bernoulli(probas)
         logprob = torch.log(probas) * action
-        #logprob = torch.gather(torch.log(probas), 1, action.T)
         loss = -logprob.sum() * td_error
         self.losses[1] = loss.item()
 
@@ -80,7 +79,10 @@ class ActorCritic:
         return td_error.detach()
 
     def get_action(self, state):
-        return torch.bernoulli(self.actor(state)).type(torch.int).tolist()
+        # print(state.size())
+        probas = self.actor(state)
+        #print(probas)
+        return torch.bernoulli(probas).type(torch.int).tolist()
 
 
 # class Policy(nn.Module):